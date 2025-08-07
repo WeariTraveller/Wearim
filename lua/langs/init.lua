9853302feb3115cb9029227d57903344bc93c706
WeariTraveller <2952706476@qq.com>
2025-08-07 09:35:32 +0800

    Trim langs/init.lua

--- lua/langs/init.lua
+++ lua/langs/init.lua
@@ -5,36 +5,32 @@ local langList = {}
 M.list = langList
 local langModuleMaps = {}
 
+-- NOTE:
+-- 1. Iter:each() will drain the iterator.
+-- 2. AI may think map() will evaluate the callback lazily (2025 Oct.6).
+--    That's incorrect. See examples/iter.lua
+-- 3. What be Iter:map()ed to nil will be filtered.
+
 M.lspIter = utils
   .getModuleNamesInDir(vim.fn.stdpath("config") .. "/lua/langs")
   -- First, this part deals with langList and langModuleMaps
   :filter(function(modul) return modul ~= "init" end)
-  -- 1. Can't use each() here, which will drain the iterator.
-  -- 2. AI may think map() will evaluate the callback lazily (2025 Oct.6).
-  --    That's incorrect. See examples/iter.lua
-  :map(
-    function(modul)
-      local langs = {}
-      for part in string.gmatch(modul, "[^&]+") do
-        table.insert(langList, part)
-        table.insert(langs, part)
-      end
-      langModuleMaps[modul] = langs
-      return modul
+  :map(function(modul)
+    local langs = {}
+    for part in string.gmatch(modul, "[^&]+") do
+      table.insert(langList, part)
+      table.insert(langs, part)
     end
-  )
-  -- Next, this part actually generates lspIter
-  :map(function(modul) return require("langs." .. modul).lsp end)
-  :filter(function(config) return config ~= nil end)
-
-M.dapIter = vim
-  .iter(langModuleMaps)
-  :map(function(modul, filetypes)
-    local config = require("langs." .. modul).dap
-    if config == nil then return nil end
-    if config.filetypes == nil then config.filetypes = filetypes end
-    return config
+    langModuleMaps[modul] = langs
+    -- Next, this part actually generates lspIter
+    return require("langs." .. modul).lsp
   end)
-  :filter(function(config) return config ~= nil end)
+
+M.dapIter = vim.iter(langModuleMaps):map(function(modul, filetypes)
+  local config = require("langs." .. modul).dap
+  if config == nil then return nil end
+  if config.filetypes == nil then config.filetypes = filetypes end
+  return config
+end)
 
 return M
