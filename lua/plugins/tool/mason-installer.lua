c180214107df5912af5bce95bebd057334b8b85e
WeariTraveller <70891059+WeariTraveller@users.noreply.github.com>
2026-05-04 19:06:25 +0800

    Fix mason won't install automatically

--- lua/plugins/tool/mason-installer.lua
+++ lua/plugins/tool/mason-installer.lua
@@ -2,10 +2,10 @@ local masonBin = vim.fn.stdpath("data") .. "/mason/bin"
 require "utils".env.PATH:append(masonBin)
 return {
   "WhoIsSethDaniel/mason-tool-installer.nvim",
-  event = "VeryLazy",
+  lazy = false,
   opts = {
     auto_update = true,
     ensure_installed = require "langs".mason,
   },
-  dependencies = { "williamboman/mason.nvim", config = true },
+  dependencies = { "williamboman/mason.nvim", opts = {} },
 }
