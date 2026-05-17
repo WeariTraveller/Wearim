7a3999d52336f1091ab8702366382781113e3c2f
WeariTraveller <70891059+WeariTraveller@users.noreply.github.com>
2026-05-17 14:46:29 +0800

    Customize alpha.nvim
    Mainly add key searching tip

--- lua/plugins/ui/alpha.lua
+++ lua/plugins/ui/alpha.lua
@@ -1,4 +1,17 @@
 return {
   "goolord/alpha-nvim",
-  config = function() require "alpha".setup(require "alpha.themes.dashboard".config) end,
+  config = function()
+    local dashboard = require "alpha.themes.dashboard"
+    local button = dashboard.button
+    dashboard.section.buttons.val = {
+      button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      button("f", "󰈞  Find file", ":Telescope find_files <CR>"),
+      button("r", "󰄉  Recent files", ":Telescope oldfiles <CR>"),
+      button("<C-h>", "󰌌  Search keymaps"),
+      button("<C-k>", "󰘳  WhichKey"),
+      button("sc / so", "󰖭  Close current / all other windows"),
+      button("<A-q>", "󰅚  Quit"),
+    }
+    require "alpha".setup(dashboard.config)
+  end,
 }
