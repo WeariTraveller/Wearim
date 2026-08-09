644da50b050e8a12ffc3eda4c0562da1cb39a624
WeariTraveller <70891059+WeariTraveller@users.noreply.github.com>
2026-08-09 17:05:45 +0800

    Edit the mod utils
    1. move wsl functions to a new module
    2. call browser without options

--- lua/utils.lua
+++ lua/utils.lua
@@ -52,16 +52,11 @@ M.check = function(prog)
   end
 end
 
-M.wslpath2Win = function(rawPath)
-  -- Raw ouput ends with \n
-  return string.sub(vim.system({ "wslpath", "-w", rawPath }):wait().stdout, 1, -2)
-end
-M.winpath2Wsl = function(rawPath) return string.sub(vim.system({ "wslpath", "-u", rawPath }):wait().stdout, 1, -2) end
-
 if isWSL then
-  function M.browseFile(path) vim.system({ vim.g.browser, "--new-tab", M.wslpath2Win(path) }) end
+  local wsl = require "wsl"
+  function M.browseFile(path) vim.system({ vim.g.browser, wsl.pathToWin(path) }) end
 else
-  function M.browseFile(path) vim.system({ vim.g.browser, "--new-tab", path }) end
+  function M.browseFile(path) vim.system({ vim.g.browser, path }) end
 end
 
 return M
---
+++ lua/wsl.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+function M.pathToWin(wslPath)
+  -- Raw ouput ends with \n
+  return string.sub(vim.system({ "wslpath", "-w", wslPath }):wait().stdout, 1, -2)
+end
+function M.pathFromWin(winPath) return string.sub(vim.system({ "wslpath", "-u", winPath }):wait().stdout, 1, -2) end
+
+function M.whereInWin(target)
+  return M.pathFromWin(string.sub(
+    -- Raw ouput ends with \r\n
+    vim.system({ "/mnt/c/Windows/System32/where.exe", "firefox" }):wait().stdout,
+    1,
+    -3
+  ))
+end
+
+return M
